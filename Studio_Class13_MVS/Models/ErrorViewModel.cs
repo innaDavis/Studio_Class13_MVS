00dde53df49060884e5112e510248aeadd96a6b8
Inna Davis <idavis@cordelllaw.com>
2019-04-17 18:22:27 -0500

    Add project files.

---
+++ Studio_Class13_MVS/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Studio_Class13_MVS.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
