0909f18016d1caf653abaa1d5d08705585cd9740
Inna Davis <idavis@cordelllaw.com>
2019-04-18 16:51:05 -0500

    commit

---
+++ Studio_Class13_MVS/Controllers/HelloController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Studio_Class13_MVS.Controllers
+{
+   
+    public class HelloController : Controller
+    { 
+        private string htmlFrom = @"<form method='get' action='/Hello/index'>";
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return Content("Some sort of content");
+        }
+    }
+}
